5368d1d726834513822defd3a40cd49833db2414
Artur <82472015+benyaar@users.noreply.github.com>
2023-03-10 23:00:13 +0200

    feat: added CRUD

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./src/schema/schema')
+const mongoose = require("mongoose");
 const app = express()
 const port = 3000
+const mongoURI = process.env.mongoURI || "mongodb+srv://admin:admin@backapi.wojaaxk.mongodb.net/?retryWrites=true&w=majority";
 
+async function runDB() {
+    try {
+        await mongoose.connect(mongoURI)
+        console.log("Connected successfully to mongoose server")
+    } catch {
+        await mongoose.disconnect()
+        console.log("Not connected")
+    }
+}
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -12,6 +23,10 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const startApp = async ()=>{
+    await runDB()
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+startApp()
